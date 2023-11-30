bbb7f4e576140c5bbe0a86a3ec248d40dbd365c0
Rafał Biolik <biorafal445@gmail.com>
2023-11-30 22:05:54 +0100

    Flex

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,4 @@
-console.log ("Ten kod jest już w repozytorium Git");
+function double(number) {
+    return number * 2;
+}
 
-let heightElement = document.querySelector(".js-height");
-let weightElement = document.querySelector(".js-weight");
-let formElement = document.querySelector(".js-form");
-let bmiElement = document.querySelector(".js-bmi");
-
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
-    let height = heightElement.value;
-    let weight = weightElement.value;
-
-    let bmi = weight / ((height / 100) ** 2);
-
-    bmiElement.innerText = bmi.toFixed(3);
-});
